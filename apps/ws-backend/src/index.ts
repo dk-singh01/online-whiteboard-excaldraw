0f9e001b18233cfd44730cb64249871ec512d7c0
divyanshusingh1101@gmail.com <divyanshusingh1101@gmail.com>
2025-04-14 15:41:07 +0000

    added websocket connection

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,25 @@
 
-
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { WebSocket, WebSocketServer } from "ws";
+import { JWT_SECRETS } from "./config";
 
 const wss = new WebSocketServer({port:8080});
 
-wss.on("connection", function connection(ws){
+wss.on("connection", function connection(ws, request){
+    const url = request.url; //ws://localhost:3000?token=1324323;
+    //url.split  => the url into ["ws://localhost:3000", "token=121323"]
+    if(!url){
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") || "";
+    const decoded = jwt.verify(token, JWT_SECRETS);
+
+    if (!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return;
+    }
     ws.on("message", function message(data){
         ws.send("pong");
     })
